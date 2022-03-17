48ecbf565638ba2e86c43d88e4bb573af0641bd7
CSIWill <91165874+CSIWill@users.noreply.github.com>
2022-03-17 11:01:01 -0400

    added views

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const path = require('path');
+const ejs = require('ejs');
+const router = express.Router;
+// const fetch = require('node-fetch');
+const dotenv = require('dotenv');
+const Rawger = require('rawger');
+dotenv.config({ path: "./config.env" });
+// const rawgapi = new (process.env.YOUR_API_KEY);
+const { json } = require('express/lib/response');
+const rawgRoutes = require('./routes/api/rawgRoutes');
+
+// Create express app
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true })); 
+
+// Initialize ejs Middleware
+app.set('view engine', 'ejs');
+app.use('/public', express.static(__dirname + '/public'));
+
+// routes
+app.get("/", (req, res) => {
+  res.render("index.ejs");
+});
+
+// router to use api
+// app.use(rawgRoutes);
+
+// Setup server ports
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server is running on ${PORT}`));
---
+++ routes/api/rawgRoutes.js
@@ -0,0 +1,2 @@
+// GET https://api.rawg.io/api/platforms?key=YOUR_API_KEY
+// GET https://api.rawg.io/api/games?key=YOUR_API_KEY&dates=2019-09-01,2019-09-30&platforms=18,1,7
