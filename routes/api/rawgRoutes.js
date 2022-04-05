a742d13ab491dacedd007c65382d64e8421f3fcd
CSIWill <91165874+CSIWill@users.noreply.github.com>
2022-04-05 15:31:14 -0400

    Game_Genre and Game_Platform data

--- routes/api/rawgRoutes.js
+++ routes/api/rawgRoutes.js
@@ -17,6 +17,99 @@ const client = new Client({
 
 client.connect();
 
+// Stores
+for(let page_num = 1; page_num < 2; page_num++) {
+  let options = {
+    method: 'GET',
+    url: 'https://api.rawg.io/api/stores?key=' + process.env.YOUR_API_KEY + '&page=' + page_num
+  };
+  axios.request(options).then(function (response) {
+    let storeData = response.data;
+    // console.log(storeData.results);
+    for (let i = 0; i < storeData.results.length; i++) {
+      let storeCreate = 'INSERT INTO STORE(STORE_ID,STORE_NAME) VALUES($1,$2)';
+      let storeInfo = [storeData.results[i].id,storeData.results[i].name];
+      client.query(storeCreate, storeInfo, (err, res) => {
+        // if (err) {
+        //   console.log(err.stack)
+        // }
+      });
+    }
+  }).catch(function (error) {
+    console.error(error);
+  });
+}
+
+// Genre
+for(let page_num = 1; page_num < 2; page_num++) {
+  let options = {
+    method: 'GET',
+    url: 'https://api.rawg.io/api/genres?key=' + process.env.YOUR_API_KEY + '&page=' + page_num
+  };
+  axios.request(options).then(function (response) {
+    let genreData = response.data;
+    // console.log(storeData.results);
+    for (let i = 0; i < genreData.results.length; i++) {
+      let genreCreate = 'INSERT INTO GENRE(GENRE_ID,GENRE_NAME) VALUES($1,$2)';
+      let genreInfo = [genreData.results[i].id,genreData.results[i].name];
+      client.query(genreCreate, genreInfo, (err, res) => {
+        // if (err) {
+        //   console.log(err.stack)
+        // }
+      });
+    }
+  }).catch(function (error) {
+    console.error(error);
+  });
+}
+
+// Developer
+for(let page_num = 1; page_num < 3; page_num++) {
+  let options = {
+    method: 'GET',
+    url: 'https://api.rawg.io/api/developers?key=' + process.env.YOUR_API_KEY + '&page=' + page_num
+  };
+  axios.request(options).then(function (response) {
+    let devData = response.data;
+    // console.log(devData.results);
+    for (let i = 0; i < devData.results.length; i++) {
+      let devCreate = 'INSERT INTO DEVELOPER(DEV_ID,DEV_NAME) VALUES($1,$2)';
+      let devInfo = [devData.results[i].id,devData.results[i].name];
+      client.query(devCreate, devInfo, (err, res) => {
+        // if (err) {
+        //   console.log(err.stack)
+        // }
+      });
+    }
+  }).catch(function (error) {
+    console.error(error);
+  });
+}
+
+// Platform
+for(let page_num = 1; page_num < 3; page_num++) {
+  let options = {
+    method: 'GET',
+    url: 'https://api.rawg.io/api/platforms?key=' + process.env.YOUR_API_KEY + '&page=' + page_num
+  };
+  axios.request(options).then(function (response) {
+    let platData = response.data;
+    // console.log(platData.results);
+    for (let i = 0; i < platData.results.length; i++) {
+      let platCreate = 'INSERT INTO PLATFORM(PLATFORM_ID,PLATFORM_NAME) VALUES($1,$2)';
+      let platInfo = [platData.results[i].id,platData.results[i].name];
+      client.query(platCreate, platInfo, (err, res) => {
+        // if (err) {
+        //   console.log(err.stack)
+        // }
+      });
+    }
+  }).catch(function (error) {
+    console.error(error);
+  });
+}
+
+// Games
 for(let page_num = 1; page_num < 26; page_num++) {
   let options = {
     method: 'GET',
@@ -41,132 +134,30 @@ for(let page_num = 1; page_num < 26; page_num++) {
         // if (err) {
         //   console.log(err.stack)
         // }
-      })      
-    //   // Populate Stores
-    //   let storeCreate = 'INSERT INTO STORE(STORE_ID,STORE_NAME) VALUES($1,$2)';
-    //   for(let j = 0; j < gameData.results[i].stores[j].length; j++) {
-    //     let storeInfo = [gameData.results[i].stores[j].store.id, gameData.results[i].stores[j].store.name];
-    //     client.query(storeCreate, storeInfo, (err, res) => {
-    //       // if (err) {
-    //       //   console.log(err.stack)
-    //       // }
-    //     })      
-    //   }
-    //   // Populate Genres
-    //   let genreCreate = 'INSERT INTO GENRE(GENRE_ID,GENRE_NAME) VALUES($1,$2)';
-    //   for(let j = 0; j < gameData.results[i].genres[j].length; j++) {
-    //     let genreInfo = [gameData[i].results[j].genre[j].id,gameData[i].results[j].genre[j].name];
-    //     client.query(genreCreate, genreInfo, (err, res) => {
-    //       // if (err) {
-    //       //   console.log(err.stack)
-    //       // }
-    //     })  
-    //   }
-    //   //Populate Developer
-    //   let devCreate = 'INSERT INTO DEVELOPER(DEV_NAME) VALUES($1)';
-
-    //   let platCreate = 'INSERT INTO PLATFORM(PLATFORM_NAME) VALUES($1)'
+      });
+      //  Game-Platform
+      for (let j=0; j<gameData.results[i].platforms.length; j++) {
+        let gameID = gameData.results[i].id;
+        let platformID = gameData.results[i].platforms[j].platform.id;
+        let gamePlatformQuery = 'INSERT INTO GAME_PLATFORM(GAMES_ID,PLATFORM_ID) VALUES($1,$2)';
+        client.query(gamePlatformQuery, [gameID,platformID], (err, res) => {
+          // if (err) {
+          //   console.log(err.stack)
+          // }
+        });
+      }
+      // Game-Genre Table
+      for (let j=0; j<gameData.results[i].genres.length; j++) {
+        let gameID = gameData.results[i].id;
+        let genreID = gameData.results[i].genres[j].id;
+        let gameGenreQuery = 'INSERT INTO GAMES_GENRE(GAMES_ID,GENRE_ID) VALUES($1,$2)';
+        client.query(gameGenreQuery, [gameID,genreID], (err, res) => {
+          // if (err) {
+          //   console.log(err.stack)
+          // }
+        });
+      }
       
-    //   for(let j = 0; j < gameData.results[i].platforms[j].platform.length; j++) {
-    //     let platInfo = [gameData.results[i].platforms[j].platform.name];
-    //     client.query(platCreate, platInfo, (err,res) => {
-    //     //   if (err) {
-    //     //   console.log(err.stack)
-    //     // }
-    //     })
-    //   }
-    }
-  }).catch(function (error) {
-    console.error(error);
-  });
-}
-
-// Stores
-for(let page_num = 1; page_num < 2; page_num++) {
-  let options = {
-    method: 'GET',
-    url: 'https://api.rawg.io/api/stores?key=' + process.env.YOUR_API_KEY + '&page=' + page_num
-  };
-  axios.request(options).then(function (response) {
-    let storeData = response.data;
-    // console.log(storeData.results);
-    for (let i = 0; i < storeData.results.length; i++) {
-      let storeCreate = 'INSERT INTO STORE(STORE_ID,STORE_NAME) VALUES($1,$2)';
-      let storeInfo = [storeData.results[i].id,storeData.results[i].name];
-      client.query(storeCreate, storeInfo, (err, res) => {
-        // if (err) {
-        //   console.log(err.stack)
-        // }
-      })
-    }
-  }).catch(function (error) {
-    console.error(error);
-  });
-}
-
-// Genre
-for(let page_num = 1; page_num < 2; page_num++) {
-  let options = {
-    method: 'GET',
-    url: 'https://api.rawg.io/api/genres?key=' + process.env.YOUR_API_KEY + '&page=' + page_num
-  };
-  axios.request(options).then(function (response) {
-    let genreData = response.data;
-    // console.log(storeData.results);
-    for (let i = 0; i < genreData.results.length; i++) {
-      let genreCreate = 'INSERT INTO GENRE(GENRE_ID,GENRE_NAME) VALUES($1,$2)';
-      let genreInfo = [genreData.results[i].id,genreData.results[i].name];
-      client.query(genreCreate, genreInfo, (err, res) => {
-        // if (err) {
-        //   console.log(err.stack)
-        // }
-      })
-    }
-  }).catch(function (error) {
-    console.error(error);
-  });
-}
-
-// Developer
-for(let page_num = 1; page_num < 3; page_num++) {
-  let options = {
-    method: 'GET',
-    url: 'https://api.rawg.io/api/developers?key=' + process.env.YOUR_API_KEY + '&page=' + page_num
-  };
-  axios.request(options).then(function (response) {
-    let devData = response.data;
-    // console.log(devData.results);
-    for (let i = 0; i < devData.results.length; i++) {
-      let devCreate = 'INSERT INTO DEVELOPER(DEV_ID,DEV_NAME) VALUES($1,$2)';
-      let devInfo = [devData.results[i].id,devData.results[i].name];
-      client.query(devCreate, devInfo, (err, res) => {
-        // if (err) {
-        //   console.log(err.stack)
-        // }
-      })
-    }
-  }).catch(function (error) {
-    console.error(error);
-  });
-}
-
-// Platform
-for(let page_num = 1; page_num < 3; page_num++) {
-  let options = {
-    method: 'GET',
-    url: 'https://api.rawg.io/api/platforms?key=' + process.env.YOUR_API_KEY + '&page=' + page_num
-  };
-  axios.request(options).then(function (response) {
-    let platData = response.data;
-    // console.log(platData.results);
-    for (let i = 0; i < platData.results.length; i++) {
-      let platCreate = 'INSERT INTO PLATFORM(PLATFORM_ID,PLATFORM_NAME) VALUES($1,$2)';
-      let platInfo = [platData.results[i].id,platData.results[i].name];
-      client.query(platCreate, platInfo, (err, res) => {
-        // if (err) {
-        //   console.log(err.stack)
-        // }
-      })
     }
   }).catch(function (error) {
     console.error(error);
@@ -180,14 +171,13 @@ router.get("/", (req, res) => {
   };
   axios.request(options).then(function (response) {
     let gameData = response.data;
-    // console.log(gameData.results[0].genres);
+    // console.log(gameData.results[0].platforms);
     res.render("index.ejs", {
       games: gameData.results,
     });
   }).catch(function (error) {
     console.error(error);
   });
-
 });
 
 router.get('/login', async (req, res) => {
@@ -204,5 +194,13 @@ router.get('/game_price', async (req, res) => {
   res.render('./html/game_price', {
   });
 });
+
+// client.query('SELECT * FROM GAMES WHERE GAMES_ID = 49',(err, res) => {
+//   if (err) {
+//     console.log(err.stack)
+//   } else {
+//     console.log(res.rows[0])
+//   }
+// });
 
 module.exports = router;
