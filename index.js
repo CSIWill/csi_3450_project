9610acdd7e66a2822eb3576f7d17c043de6ee579
JSLee311 <stevenlee@oakland.edu>
2022-04-10 03:17:07 -0400

    moved code

--- index.js
+++ index.js
@@ -1,18 +1,17 @@
 const express = require('express');
 const path = require('path');
 const ejs = require('ejs');
-// const router = express.Router;
 const session = require("express-session");
 const flash = require("express-flash");
 const passport = require("passport");
 const bcrypt = require("bcrypt");
-// const fetch = require('node-fetch');
 const dotenv = require('dotenv');
 dotenv.config({ path: "./config.env" });
-// const rawgapi = new (process.env.YOUR_API_KEY);
 const axios = require("axios").default;
 const { json } = require('express/lib/response');
 const rawgRoutes = require('./routes/api/rawgRoutes');
+const loginRoutes = require('./routes/loginRoutes');
+
 // Create express app
 const app = express();
 
@@ -21,20 +20,17 @@ app.use(express.urlencoded({ extended: true }));
 
 // Initialize ejs Middleware
 app.set('view engine', 'ejs');
-// app.use(express.urlencoded({ extended: false }));
 app.use('/public', express.static(__dirname + '/public/'));
 
 // routes
 
 // router to use api
 app.use(rawgRoutes);
+app.use(loginRoutes);
 
 // connect to database
 const { Client } = require('pg');
-const initializePassport = require("./passportConfig");
 const res = require('express/lib/response');
-
-initializePassport(passport);
 
 const client = new Client({
   connectionString: process.env.DATABASE_URL,
@@ -48,120 +44,20 @@ client.connect();
 // Setup server ports
 const PORT = process.env.PORT || 3000;
 
-app.use(
-  session({
-    secret: "secret",
-    resave: false,
-    saveUninitialized: false
-  })
-);
-
-app.use(passport.initialize());
-app.use(passport.session());
-
-app.use(flash());
-
 app.get("/", (req, res) => {
   res.render("index");
 });
 
-app.get("/users/register", checkAuthenticated, (req, res) => {
+app.get("/users/register", (req, res) => {
   res.render("register");
 })
 
-app.get("/users/login", checkAuthenticated, (req, res) => {
+app.get("/users/login", (req, res) => {
   res.render("login");
 })
 
-app.get("/users/dashboard", checkNotAuthenticated, (req, res) => {
-  res.render("dashboard", { user: req.user.email });
+app.get("/users/dashboard", (req, res) => {
+  res.render("dashboard");
 })
-
-app.get("/users/logout", (req, res) => {
-  req.logOut();
-  req.flash("success_msg", "You have successfully logged out");
-  res.redirect("/users/login");
-})
-
-app.post("/users/register", async (req, res) => {
-  let { email, password, password2 } = req.body;
-
-  console.log({
-    email,
-    password,
-    password2
-  });
-
-  let errors = [];
-
-  if (!email || !password || !password2) {
-    errors.push({ message: "Please enter all fields" });
-  }
-
-  if (password.length < 6) {
-    errors.push({ message: "Password should be at least 6 characters" });
-  }
-  if (password != password2) {
-    errors.push({ message: "Passwords do not match" });
-  }
-
-  if (errors.length > 0) {
-    res.render("register", { errors });
-  } else {
-    let hashedPassword = await bcrypt.hash(password, 10);
-    console.log(hashedPassword);
-
-    client.query(
-      `SELECT * FROM users
-          WHERE user_email = $1`, [email], (err, results) => {
-      if (err) {
-        throw err;
-      };
-      console.log(results.rows);
-
-      if (results.rows.length > 0) {
-        errors.push({ message: "Email already registered" });
-        res.render("register", { errors });
-      } else {
-        client.query(
-          `INSERT INTO users (user_email, user_password)
-                      VALUES ($1, $2)
-                      RETURNING user_id, user_password`,
-          [email, hashedPassword],
-          (err, results) => {
-            if (err) {
-              throw err;
-            }
-            console.log(results.rows);
-            req.flash('success_msg', "You are now registered. Pleae log in");
-            res.redirect("/users/login");
-          }
-        );
-      }
-    }
-    );
-  }
-});
-
-app.post("/users/login",
-  passport.authenticate('local', {
-    successRedirect: "/",
-    failureRedirect: "/users/login",
-    failureFlash: true
-  })
-);
-
-function checkAuthenticated(req, res, next) {
-  if (req.isAuthenticated()) {
-    return res.redirect("/users/dashboard");
-  }
-  return next();
-}
-function checkNotAuthenticated(req, res, next) {
-  if (req.isAuthenticated()) {
-    return next();
-  }
-  res.redirect("/users/login");
-}
 
 app.listen(PORT, () => console.log(`Server is running on ${PORT}`));
