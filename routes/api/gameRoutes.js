623220a4fa802eb37d30255e71be46841771b72f
CSIWill <91165874+CSIWill@users.noreply.github.com>
2022-04-10 20:54:36 -0400

    add one genre, one platform

--- routes/api/gameRoutes.js
+++ routes/api/gameRoutes.js
@@ -46,7 +46,6 @@ router.get('/advancedSearch', async (req, res) => {
 
 router.post('/search/', async (req, response) => {
   let userQuery = await ['%' + req.body.userSearch + '%'];
-
   let searchNameQuery = 'SELECT * FROM GAMES WHERE GAMES_TITLE ILIKE $1';
   let searchResults = [];
   client.query(searchNameQuery, userQuery, async (err, res) => {
@@ -64,20 +63,59 @@ router.post('/search/', async (req, response) => {
 
 router.post('/detailSearch/', async (req, response) => {
   let userQuery = await ['%' + req.body.userSearch + '%'];
-  let searchNameQuery = 'SELECT * FROM GAMES WHERE GAMES_TITLE ILIKE $1';
+  await console.log(req.body);
+  let query1 = 'DROP VIEW IF EXISTS GAME_SEARCH';
+  // let searchQuery = 'CREATE VIEW GAME_SEARCH AS SELECT * FROM GAMES NATURAL JOIN GAMES_GENRE NATURAL JOIN GAME_PLATFORM NATURAL JOIN GENRE NATURAL JOIN PLATFORM WHERE GAMES_TITLE ILIKE $1';
+  let viewQuery = 'SELECT * FROM GAME_SEARCH';
+  let searchQuery = 'SELECT * FROM GAMES NATURAL JOIN GAMES_GENRE NATURAL JOIN GAME_PLATFORM NATURAL JOIN GENRE NATURAL JOIN PLATFORM WHERE GAMES_TITLE ILIKE $1';
   let searchResults = [];
-  client.query(searchNameQuery, userQuery, async (err, res) => {
+  if (await req.body.action == 'on') {
+    searchQuery = searchQuery + `AND GENRE_NAME = 'Action'`;
+  };
+  if (await req.body.adventure == 'on') {
+    searchQuery = searchQuery + `AND GENRE_NAME = 'Adventure'`;
+  };
+  if (await req.body.platformer == 'on') {
+    searchQuery = searchQuery + `AND GENRE_NAME = 'Platformer'`;
+  };
+  if (await req.body.rpg == 'on') {
+    searchQuery = searchQuery + `AND GENRE_NAME = 'RPG'`;
+  };
+  if (await req.body.shooter == 'on') {
+    searchQuery = searchQuery + `AND GENRE_NAME = 'Shooter'`;
+  };
+  if (await req.body.pc == 'on') {
+    searchQuery = searchQuery + `AND PLATFORM_NAME = 'PC'`;
+  };
+  if (await req.body.xbox_one == 'on') {
+    searchQuery = searchQuery + `AND PLATFORM_NAME = 'Xbox One'`;
+  };
+  if (await req.body.xbox_series == 'on') {
+    searchQuery = searchQuery + `AND PLATFORM_NAME = 'Xbox Series S/X'`;
+  };
+  if (await req.body.ps5 == 'on') {
+    searchQuery = searchQuery + `AND PLATFORM_NAME = 'PlayStation 5'`;
+  };
+  if (await req.body.ps4 == 'on') {
+    searchQuery = searchQuery + `AND PLATFORM_NAME = 'PlayStation 4'`;
+  };
+  if (await req.body.nintendo_switch == 'on') {
+    searchQuery = searchQuery + `AND PLATFORM_NAME = 'Nintendo Switch'`;
+  };
+  client.query(searchQuery, userQuery, async (err, res) => {
     if (err) {
       console.log(err.stack)
       searchResults = ['No Results Found', 'No Results Found', 'No Results Found'];
     } else {
       searchResults = res.rows;
+      console.log(searchResults);
       response.render('./html/results', {
         games: searchResults,
       });
     }
   })
 });
+
 
 router.get('/game_info/:id', function (req, response) {
   // console.log(req.params.id);
@@ -93,7 +131,6 @@ router.get('/game_info/:id', function (req, response) {
         client.query(query4, async (err, res4) => {
           client.query(queryGenre, async (err, res5) => {
             client.query(query6, async (err, res6) => {
-              // console.log(res6.rows);
               response.render('./html/game_info', {
                 games: res3.rows,
                 games_genre: res6.rows
