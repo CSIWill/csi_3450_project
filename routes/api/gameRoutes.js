189967cf07a20aa890ff387b633bc07a10d4156f
CSIWill <91165874+CSIWill@users.noreply.github.com>
2022-04-12 00:40:00 -0400

    remove, view wishlit items and sort

--- routes/api/gameRoutes.js
+++ routes/api/gameRoutes.js
@@ -77,10 +77,10 @@ router.post('/detailSearch/', async (req, response) => {
     req.body.genre = [req.body.genre];
   }
   if (req.body.platform == undefined) {
-    req.body.platform =[''] ;
+    req.body.platform = [''];
   }
   if (req.body.genre == undefined) {
-    req.body.genre =[''] ;
+    req.body.genre = [''];
   }
   // console.log(Array.isArray(req.body.platform));
   // console.log(req.body.platform);
@@ -93,14 +93,14 @@ router.post('/detailSearch/', async (req, response) => {
       advancedSearchQuery = 'SELECT GAMES_ID FROM GAMES NATURAL JOIN GAMES_GENRE NATURAL JOIN GENRE NATURAL JOIN GAME_PLATFORM NATURAL JOIN PLATFORM WHERE GAMES_TITLE ILIKE $1 AND (GENRE_NAME ILIKE ' + await ["'%" + req.body.genre[0] + "%'"];
       for (let i = 1; i < req.body.genre.length; i++) {
         let nextGenre = await ['"%' + req.body.genre[i] + '%"'];
-        advancedSearchQuery = advancedSearchQuery + ' OR GENRE_NAME ILIKE ' +  await ["'%" + req.body.genre[i] + "%'"];
+        advancedSearchQuery = advancedSearchQuery + ' OR GENRE_NAME ILIKE ' + await ["'%" + req.body.genre[i] + "%'"];
       }
       advancedSearchQuery = advancedSearchQuery + ')';
       if (req.body.platform.length > 0) {
         advancedSearchQuery = advancedSearchQuery + ' AND (PLATFORM_NAME ILIKE ' + await ["'%" + req.body.platform[0] + "%'"];
         for (let i = 1; i < req.body.platform.length; i++) {
           let nextGenre = await ['"%' + req.body.platform[i] + '%"'];
-          advancedSearchQuery = advancedSearchQuery + ' OR PLATFORM_NAME ILIKE ' +  await ["'%" + req.body.platform[i] + "%'"];
+          advancedSearchQuery = advancedSearchQuery + ' OR PLATFORM_NAME ILIKE ' + await ["'%" + req.body.platform[i] + "%'"];
         }
         advancedSearchQuery = advancedSearchQuery + ')';
       }
@@ -108,17 +108,17 @@ router.post('/detailSearch/', async (req, response) => {
   }
   if (req.body.search_type == 'developer') {
     advancedSearchQuery = 'SELECT * FROM GAMES WHERE GAMES_TITLE ILIKE $1';
-    advancedSearchQuery = 'SELECT GAMES_ID FROM GAMES NATURAL JOIN GAMES_DEVELOPER NATURAL JOIN DEVELOPER NATURAL JOIN GAME_PLATFORM NATURAL JOIN PLATFORM NATURAL JOIN GAMES_GENRE NATURAL JOIN GENRE WHERE DEV_NAME ILIKE $1 AND (GENRE_NAME ILIKE '+ await ["'%" + req.body.genre[0] + "%'"]; 
+    advancedSearchQuery = 'SELECT GAMES_ID FROM GAMES NATURAL JOIN GAMES_DEVELOPER NATURAL JOIN DEVELOPER NATURAL JOIN GAME_PLATFORM NATURAL JOIN PLATFORM NATURAL JOIN GAMES_GENRE NATURAL JOIN GENRE WHERE DEV_NAME ILIKE $1 AND (GENRE_NAME ILIKE ' + await ["'%" + req.body.genre[0] + "%'"];
     for (let i = 1; i < req.body.genre.length; i++) {
       let nextGenre = await ['"%' + req.body.genre[i] + '%"'];
-      advancedSearchQuery = advancedSearchQuery + ' OR GENRE_NAME ILIKE ' +  await ["'%" + req.body.genre[i] + "%'"];
+      advancedSearchQuery = advancedSearchQuery + ' OR GENRE_NAME ILIKE ' + await ["'%" + req.body.genre[i] + "%'"];
     }
     advancedSearchQuery = advancedSearchQuery + ')';
     if (req.body.platform.length > 0) {
       advancedSearchQuery = advancedSearchQuery + ' AND (PLATFORM_NAME ILIKE ' + await ["'%" + req.body.platform[0] + "%'"];
       for (let i = 1; i < req.body.platform.length; i++) {
         let nextGenre = await ['"%' + req.body.platform[i] + '%"'];
-        advancedSearchQuery = advancedSearchQuery + ' OR PLATFORM_NAME ILIKE ' +  await ["'%" + req.body.platform[i] + "%'"];
+        advancedSearchQuery = advancedSearchQuery + ' OR PLATFORM_NAME ILIKE ' + await ["'%" + req.body.platform[i] + "%'"];
       }
       advancedSearchQuery = advancedSearchQuery + ')';
     }
@@ -233,21 +233,89 @@ router.post('/advancedSort/', async (req, response) => {
   });
 });
 
-router.post('/addWishlist/', async (req,response) => {
-  console.log(req.body);
-  let findUserID = 'SELECT USER_ID FROM USERS WHERE USER_EMAIL ILIKE' + await ["'%" + req.body.user + "%'"];
-  client.query(findUserID, async (err,res) => {
+let globalUserID = 0;
+let globalWishlistQuery = 'SELECT * FROM WISHLIST NATURAL JOIN GAMES NATURAL JOIN PLATFORM WHERE USER_ID = $1';
+router.post('/addWishlist/', async (req, response) => {
+  // console.log(req.body);
+  let findUserID = 'SELECT USER_ID FROM USERS WHERE USER_EMAIL =' + await ["'" + req.body.user + "'"];
+  client.query(findUserID, async (err, res) => {
     if (err) {
       console.log(err.stack);
     } else {
-      console.log(res.rows[0].user_id);
+      // console.log(res.rows[0].user_id);
+      globalUserID = res.rows[0].user_id;
       let addWishlistQuery = 'INSERT INTO WISHLIST(USER_ID, GAMES_ID, PLATFORM_ID) VALUES($1,$2,$3)';
       let addWishlistEntry = [res.rows[0].user_id, req.body.game_id_num, req.body.platform_request];
-      client.query(addWishlistQuery, addWishlistEntry);
+      client.query(addWishlistQuery, addWishlistEntry, async (err, res1) => {
+        let wishlistQuery = 'SELECT * FROM WISHLIST NATURAL JOIN GAMES NATURAL JOIN PLATFORM WHERE USER_ID = ' + res.rows[0].user_id;
+        client.query(wishlistQuery, (err, res2) => {
+          // console.log(res2.rows);
+          response.render('./html/wishlist', {
+            games: res2.rows,
+          });
+        });
+      });
     }
   });
-  
 });
-
+router.get('/requestWishlist', async (req, res) => {
+  res.render('./html/requestWishlist');
+});
+router.post('/viewWishlist/', async (req, response) => {
+  let findUserID = 'SELECT USER_ID FROM USERS WHERE USER_EMAIL =' + await ["'" + req.body.user + "'"];
+  client.query(findUserID, async (err, res) => {
+    if (err) {
+      console.log(err.stack);
+    } else {
+      globalUserID = res.rows[0].user_id;
+      let wishlistQuery = 'SELECT * FROM WISHLIST NATURAL JOIN GAMES NATURAL JOIN PLATFORM WHERE USER_ID = ' + res.rows[0].user_id;
+      client.query(wishlistQuery, (err, res1) => {
+        response.render('./html/wishlist', {
+          games: res1.rows,
+        });
+      });
+    }
+  });
+});
+router.post('/delWishlistItem/', async (req, response) => {
+  let delUser = await ["'" + req.body.user_id_num + "'"];
+  globalUserID = req.body.user_id_num;
+  let delEntry = [req.body.user_id_num , req.body.games_id_num, req.body.platform_id_num];
+  // console.log(delEntry);
+  let delQuery = 'DELETE FROM WISHLIST WHERE USER_ID = $1 AND GAMES_ID = $2 AND PLATFORM_ID = $3';
+  client.query(delQuery,delEntry, (err,res) => {
+    let wishlistQuery = 'SELECT * FROM WISHLIST NATURAL JOIN GAMES NATURAL JOIN PLATFORM WHERE USER_ID = ' + delUser;
+      client.query(wishlistQuery, (err, res1) => {
+        // console.log(res1.rows);
+        response.render('./html/wishlist', {
+          games: res1.rows,
+        });
+      });
+  });
+});
+router.post('/wishlistSort/', async (req, response) => {
+  let wishlistQuery = '';
+  if (req.body.sort_type == 'alphabetical' && req.body.sort_order == 'ascending') {
+    wishlistQuery = globalWishlistQuery + ' ORDER BY GAMES_TITLE ASC';
+  }
+  if (req.body.sort_type == 'alphabetical' && req.body.sort_order == 'descending') {
+    wishlistQuery = globalWishlistQuery + ' ORDER BY GAMES_TITLE DESC';
+  }
+  if (req.body.sort_type == 'metacritic_score' && req.body.sort_order == 'ascending') {
+    wishlistQuery = globalWishlistQuery + ' AND GAMES_SCORE IS NOT NULL ORDER BY GAMES_SCORE ASC';
+  }
+  if (req.body.sort_type == 'metacritic_score' && req.body.sort_order == 'descending') {
+    wishlistQuery = globalWishlistQuery + ' AND GAMES_SCORE IS NOT NULL ORDER BY GAMES_SCORE DESC';
+  }
+  client.query(wishlistQuery, [globalUserID], async (err, res) => {
+    if (err) {
+      console.log(err.stack);
+    } else {
+      response.render('./html/wishlist', {
+        games: res.rows,
+      });
+    }
+  });
+});
 
 module.exports = router;
